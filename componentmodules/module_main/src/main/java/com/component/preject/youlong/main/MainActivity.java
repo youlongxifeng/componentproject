c97a5a7e6d10c5628bd5b2d519fd1658632d8a21
youlongxifeng <xie774505257@163.com>
2018-12-05 16:04:20 +0800

    提交代码
    提交代码

--- componentmodules/module_main/src/main/java/com/component/preject/youlong/main/MainActivity.java
+++ componentmodules/module_main/src/main/java/com/component/preject/youlong/main/MainActivity.java
@@ -3,6 +3,8 @@ package com.component.preject.youlong.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import butterknife.BindView;
 
 /**
  * @Author: xiezhenggen
