1510d422b0cf7f49fc3098bab30ea1baa04f5cfc
youlongxifeng <xie774505257@163.com>
2018-12-05 16:18:03 +0800

    ButterKnife  生成R2的步骤  Android开发，在library中使用ButterKnife，需要做一些配置否则会遇到一些问题。  我开发中遇到的问题是，通过@BindView(R.id.xxx)查找控件时，会报“Attribute value must be constant”的异常。  解决方案如下：
     在根目录build.gradle文件的dependencies目录下添加如下:
     classpath 'com.jakewharton:butterknife-gradle-plugin:8.4.0'
     在module的build.gradle文件的顶部加入apply plugin: 'com.jakewharton.butterknife'
     在module的build.gradle文件的dependencies目录下添加如下：
     implementation 'com.jakewharton:butterknife:8.8.1'
     annotationProcessor 'com.jakewharton:butterknife-compiler:8.8.1'
     在module中使用R2代替R来findViewById即可. 鼠标放在R.layout.activity_main上点击鼠标右键→Generate即可看到Generate ButterKnife Injections
     @BindView(R2.id.xxx)

--- componentmodules/module_main/src/main/java/com/component/preject/youlong/main/MainActivity.java
+++ componentmodules/module_main/src/main/java/com/component/preject/youlong/main/MainActivity.java
@@ -3,8 +3,10 @@ package com.component.preject.youlong.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @Author: xiezhenggen
@@ -13,9 +15,16 @@ import butterknife.BindView;
  */
 public class MainActivity extends AppCompatActivity {
 
+
+    @BindView(R2.id.my_text)
+    TextView myText;
+    @BindView(R2.id.my_ll)
+    LinearLayout myLl;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_main);
 
     }
